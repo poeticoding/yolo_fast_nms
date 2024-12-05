b0ef6b279ffcb608957996f2da190ae74d4d928c
Alvise Susmel <alvise@poeticoding.com>
2024-12-05 18:16:38 +0100

    v0.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## v0.1.1 (2024-12-05)
+
+### Bug fixes
+
+  * [Rustler] Fixed compilation issues
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule YoloFastNMS.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/poeticoding/yolo_fast_nms"
-  @version "0.1.0"
+  @version "0.1.1"
 
   def project do
     [
